d979e7907e67d5f3baa919780b26e45b232117f1
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-02-10 17:42:04 +0000

    add pizza component to app
    - map through pizza data

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import pizzas from '../data/pizza.json'
+import Pizza from './Pizza';
 
 function App() {
-  return <div>Hello</div>
+  return <ul>{pizzas.map(pizza => <Pizza key={pizza.id} pizza={pizza} />)}</ul>
 }
 
 export default App;
