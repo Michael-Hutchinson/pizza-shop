3c6596980df041f82b6e9463e11d379b3a2b990e
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-02-10 18:34:40 +0000

    import company logo

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import pizzas from '../data/pizza.json'
+import pizzas from '../data/pizza.json';
 import Pizza from './Pizza';
+import logo from '../images/pizzaLogo.png';
 
 function App() {
   return (
     <div className="container">
+      <img src={logo} alt="Logo" />
       <ul>{pizzas.map(pizza => <Pizza key={pizza.id} pizza={pizza} />)}</ul>
     </div>
   )
